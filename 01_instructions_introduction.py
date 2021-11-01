3603b1758817074c6719c8b7678a17fe3bd3583e
roawan <86275735+roawan@users.noreply.github.com>
2021-11-02 12:59:47 +1300

    changed theme to math, instructions compulsory

---
+++ 01_instructions_introduction.py
@@ -0,0 +1,3 @@
+name = input("What is your name?")
+name = name.title()
+print("Hello {} \nYou will be asked with 5 questions involving basic math.\nDo not write the answer, instead input the number of your desired answer, listed when asked.\nGood luck".format(name))
