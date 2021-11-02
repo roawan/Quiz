6a56aabd1e4d3378e0880e13c1f5c0165325c582
roawan <86275735+roawan@users.noreply.github.com>
2021-11-02 13:00:35 +1300

    created new questions + initial commit

---
+++ 02_questions.py
@@ -0,0 +1,87 @@
+score = 0
+score = int(score)
+
+print("""What is the Square Root of 100?
+1.  11
+2.  10
+3.  5
+4.  14.14""")
+
+answer1 = "2"
+response1 = input("Your answer is: ")
+
+if (response1 != answer1):
+    print("Sorry, that is incorrect!")
+else:
+    print("Well done! " + response1 + " is correct!")
+    score = score + 1
+
+print("Your current score is " + str(score) + " out of 5")
+
+
+print("""5 x 7 =?
+1. 35 
+2. 30 
+3. 75 
+4. 10""")
+
+answer2 = "1"
+response2 = input("Your answer is:")
+
+if (response2 != answer2):
+    print("Sorry, that is incorrect!")
+else:
+    print("Well done! " + response2 + " is correct!")
+    score = score + 1
+
+print("Your current score is " + str(score) + " out of 5")
+
+
+print("""8 x (9 + 3) =?
+1.  98
+2.  108
+3.  106
+4.  96""")
+
+answer3 = "4"
+response3 = input("Your answer is:")
+
+if (response3 != answer3):
+    print("Sorry, that is incorrect!")
+else:
+    print("Well done! " + response3 + " is correct!")
+    score = score + 1
+
+print("Your current score is " + str(score) + " out of 5")
+
+
+print("""76 - 42
+1. 34
+2. 32
+3. 44
+4. 30""")
+
+answer4 = "1"
+response4 = input("Your answer is:")
+
+if (response4 != answer4):
+    print("Sorry, that is incorrect!")
+else:
+    print("Well done! " + response4 + " is correct!")
+    score = score + 1
+
+print("Your current score is " + str(score) + " out of 5")
+
+
+print(""" The Hypotenuse times sin(𝛳) = the length of the opposite
+1. False
+2. True""")
+
+answer5 = "2"
+response5 = input("Your answer is:")
+
+if (response5 != answer5):
+    print("Sorry, that is incorrect!")
+else:
+    print("Well done! " + response5 + " is correct!")
+    score = score + 1
