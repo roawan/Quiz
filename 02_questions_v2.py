c8e9feb963716460d503c5c95097bf54a9b2b1a0
roawan <86275735+roawan@users.noreply.github.com>
2021-11-02 13:14:06 +1300

    changed numbered answers, and updated instructions

---
+++ 02_questions_v2.py
@@ -0,0 +1,83 @@
+score = 0
+score = int(score)
+
+print("""What is the Square Root of 100?
+a.  11
+b.  10
+c.  5
+d.  14.14""")
+
+answer1 = "b"
+response1 = input("Your answer is: ")
+
+if (response1 != answer1):
+    print("Sorry, that is incorrect!")
+else:
+    print("Well done! " + response1 + " is correct!")
+    score = score + 1
+
+print("Your current score is " + str(score) + " out of 5")
+
+print("""5 x 7 =?
+a. 35 
+b. 30 
+c. 75 
+d. 10""")
+
+answer2 = "a"
+response2 = input("Your answer is:")
+
+if (response2 != answer2):
+    print("Sorry, that is incorrect!")
+else:
+    print("Well done! " + response2 + " is correct!")
+    score = score + 1
+
+print("Your current score is " + str(score) + " out of 5")
+
+print("""8 x (9 + 3) =?
+a.  98
+b.  108
+c.  106
+d.  96""")
+
+answer3 = "d"
+response3 = input("Your answer is:")
+
+if (response3 != answer3):
+    print("Sorry, that is incorrect!")
+else:
+    print("Well done! " + response3 + " is correct!")
+    score = score + 1
+
+print("Your current score is " + str(score) + " out of 5")
+
+print("""76 - 42
+a. 34
+b. 32
+c. 44
+d. 30""")
+
+answer4 = "a"
+response4 = input("Your answer is:")
+
+if (response4 != answer4):
+    print("Sorry, that is incorrect!")
+else:
+    print("Well done! " + response4 + " is correct!")
+    score = score + 1
+
+print("Your current score is " + str(score) + " out of 5")
+
+print(""" The Hypotenuse times sin(𝛳) = the length of the opposite
+a. False
+b. True""")
+
+answer5 = "b"
+response5 = input("Your answer is:")
+
+if (response5 != answer5):
+    print("Sorry, that is incorrect!")
+else:
+    print("Well done! " + response5 + " is correct!")
+    score = score + 1
